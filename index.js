55451dac31aaccd5ca90621b1a358915acfbbd65
Mohamad Tassieh <mohamad.bassam.tessieh@gmail.com>
2021-01-31 09:48:51 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -72,12 +72,3 @@ function calculatePayroll (array){
  let grandTotalOwed = array.reduce((m, e) => m + allWagesFor.call(e), 0);
  return grandTotalOwed;
 }
-
-
-/*
- We're giving you this function. Take a look at it, you might see some usage
- that's new and different. That's because we're avoiding a well-known, but
- sneaky bug that we'll cover in the next few lessons!
- As a result, the lessons for this function will pass *and* it will be available
- for you to use if you need it!
- */
